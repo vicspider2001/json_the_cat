6b07dff2ae0ffafc1691aad685f5159a5a9f65db
Victor Anogwih <anogwihvictor@gmail.com>
2023-12-19 23:30:31 -0500

    Completed Assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,28 @@
 const request = require('request');
 
-const query = process.argv.slice(2);
-
-if (!query) {
-  console.log('Please provide breed name.');
-  process.exit(1);
-}
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${query}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error finding breed name:', error.message);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to get the resource. Status code: ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  const data = JSON.parse(body);
-  if(data.length === 0) {
-    console.log(`Breed ${query} was not found. We will make efforts to include more breeds`)
-  }
-  data.map((item) => {
-    console.log(item.description);
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+      
+    }
+  
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      const errorMsg = `Breed ${breedName} was not found. We will make efforts to include more breeds`;
+      callback(errorMsg, null);
+    } else {
+      data.map((item) => {
+        const description = item.description;
+        callback(null, description);
+      });
+    }
+    
   });
-});
+};
+module.exports = { fetchBreedDescription };
+
+
+
